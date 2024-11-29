629e43a96c3395e4f9a2098f2ca09e62a9d0987b
1p22geo <1p22geodecki@gmail.com>
2024-11-29 16:48:29 +0100

    feat: MORE APIs

---
+++ src/app/api/order/[id]/add/route.ts
@@ -0,0 +1,43 @@
+import { UserRouteResponse } from "@ck/app/api/user/route";
+import { addProductToOrder } from "@ck/lib/addProduct";
+import { MongoClient, ObjectId } from "mongodb";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { env } from "process";
+
+
+export const GET = async (_: NextRequest, { params }: { params: Promise<{ id: string }> }) => {
+  const uri = env.MONGODB_URI
+    ? env.MONGODB_URI
+    : (() => {
+      throw Error("no mongodb URI, set MONGODB_URI environment variable");
+    })();
+  const client = new MongoClient(uri);
+  await client.connect();
+
+
+  const sess_id = (await cookies()).get("session")?.value
+  let login: null | UserRouteResponse = null;
+  if (sess_id) {
+    login = await (
+      await fetch(`${env.SELF_URI ?? "localhost:3000"}/api/user`, {
+        headers: { Cookie: `session=${sess_id}` },
+      })
+    ).json();
+  }
+  if (!login) {
+    return NextResponse.json({
+      "status": "error",
+      "error": "no session cookie"
+    }, { status: 401 })
+  }
+
+  const id = (await params).id
+
+  await addProductToOrder(new ObjectId(id), login.user._id)
+
+  await client.close();
+  return NextResponse.json({
+    "status": "added"
+  }, { "status": 201 });
+};
